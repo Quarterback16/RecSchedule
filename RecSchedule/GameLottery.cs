11cbbb3b8008f5744f42abd6a1575201f3012032
Steve Colonna <quarterback16@grapevine.com.au>
2020-02-29 16:39:53 +1100

    Add more games

--- RecSchedule/GameLottery.cs
+++ RecSchedule/GameLottery.cs
@@ -11,31 +11,61 @@ namespace RecSchedule
 				new Entrant
 				{
 					Name = "[[FTL]]",
-					Balls = 3
+					Balls = 4
 				},
 				new Entrant
 				{
 					Name = "[[GalacticCivilizations_3]]",
-					Balls = 3
+					Balls = 5
 				},
 				new Entrant
 				{
 					Name = "[[Anno_2025]]",
-					Balls = 2
+					Balls = 3
 				},
 				new Entrant
 				{
 					Name = "[[Stellaris]]",
-					Balls = 1
+					Balls = 2
 				},
 				new Entrant
 				{
 					Name = "Defence Grid 2",
-					Balls = 1
+					Balls = 2
 				},
 				new Entrant
 				{
 					Name = "Ultima IV",
+					Balls = 1
+				},
+				new Entrant
+				{
+					Name = "Darkest Dungeon",
+					Balls = 1
+				},
+				new Entrant
+				{
+					Name = "Divinity Original Sin",
+					Balls = 1
+				},
+				new Entrant
+				{
+					Name = "Endless Legend",
+					Balls = 1
+				},
+				new Entrant
+				{
+					Name = "Endless Space",
+					Balls = 1
+				},
+				new Entrant
+				{
+					Name = "Master of Orion 2",
+					Balls = 1
+				},
+				new Entrant
+				{
+					Name = "Alpha Centauri",
 					Balls = 1
 				},
 			};
